1d85a8b5a2cace4e17730c08b7beecfdd4eb19e5
Niels Thykier <nt@asseco.dk>
2021-06-18 10:40:36 +0200

    OVS: Add test data
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ datamodel/testdata.d/07_test_data_ovs.sql
@@ -0,0 +1,14 @@
+\set ON_ERROR_STOP true
+\connect dcsa_openapi
+
+BEGIN;
+
+CREATE TEMP TABLE tmp_carrier AS SELECT * FROM dcsa_im_v3_0.carrier LIMIT 0;
+ALTER TABLE tmp_carrier DROP COLUMN id;
+
+\copy tmp_carrier from '../referencedata.d/sample_carriers.csv' CSV HEADER
+
+INSERT INTO dcsa_im_v3_0.carrier (carrier_name, smdg_code, nmfta_code)
+SELECT carrier_name, smdg_code, nmfta_code FROM tmp_carrier;
+
+COMMIT;
