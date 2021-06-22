7c65a0d5bbc211ce8eb43d5958150d6bba30eccc
Niels Thykier <nt@asseco.dk>
2021-06-22 16:01:58 +0200

    Include country codes and UN Locodes
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/testdata.d/07_test_data_ovs.sql
+++ datamodel/testdata.d/07_test_data_ovs.sql
@@ -11,4 +11,7 @@ ALTER TABLE tmp_carrier DROP COLUMN id;
 INSERT INTO dcsa_im_v3_0.carrier (carrier_name, smdg_code, nmfta_code)
 SELECT carrier_name, smdg_code, nmfta_code FROM tmp_carrier;
 
+\copy dcsa_im_v3_0.country from '../referencedata.d/countrycodes.csv' CSV HEADER
+\copy dcsa_im_v3_0.un_location from '../referencedata.d/unlocationcodes.csv' CSV HEADER
+
 COMMIT;
